79e85d8b3e18c867e1e80c359a96896641178f70
Charles Chen <117331377+cookacola@users.noreply.github.com>
2023-07-26 22:03:04 -0500

    Font change

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Contact from "@/components/Contact";
 import { getContact } from "../../../sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
+import { Raleway } from "next/font/google";
+
+const raleway = Raleway({ weight: "400", subsets: ["latin"] });
 
 async function ContactPage() {
 	const contact = await getContact();
@@ -9,7 +12,7 @@ async function ContactPage() {
 		<section className="grid items-start grid-cols-1 gap-6 p-6 md:grid-cols-2">
 			<div className="p-6 space-y-4 shadow-md">
 				<h1 className="text-2xl font-bold text-center">{contact.header}</h1>
-				<p className="text-lg text-white">
+				<p className={`${raleway.className} text-lg text-white`}>
 					<PortableText value={contact.content} />
 				</p>
 			</div>
--- src/components/Profile/ProfileBio.jsx
+++ src/components/Profile/ProfileBio.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Link from "next/link";
+import { Raleway } from "next/font/google";
 import { getProfile } from "../../../sanity/sanity-utils";
 import { PortableText } from "@portabletext/react";
+
+const raleway = Raleway({ weight: "400", subsets: ["latin"] });
 
 async function ProfileBio({ isHome }) {
 	const profile = await getProfile();
 	return (
 		<div className="flex flex-col justify-center w-1/2 bioSection">
 			<h2 className="mb-4 text-2xl font-bold">{profile.header}</h2>
-			<p className="mb-4 text-base font-normal text-gray-100">
+			<p
+				className={`${raleway.className} mb-4 text-base font-normal text-gray-100`}
+			>
 				<PortableText value={profile.content} />
 			</p>
 			{isHome ? <Link href="/about">More</Link> : null}
